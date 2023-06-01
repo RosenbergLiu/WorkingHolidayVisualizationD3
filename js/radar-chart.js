3621a2238a5c4bb94b4bbc35a17fe1624e419d60
RoshanLiu <xygenio@hotmail.com>
2023-06-02 00:03:10 +1000

    finish radar

--- js/radar-chart.js
+++ js/radar-chart.js
@@ -1,15 +1,39 @@
 async function drawRadarGraph() {
     
     let data = await d3.csv("cleaned_data/summary.csv");
+    let entities = Array.from(new Set(data.map(d => d.entity)));
+    let checkboxes = d3.select("#checkboxes")
+        .selectAll("div")
+        .data(entities)
+        .enter()
+        .append("div")
+        .attr("class", "form-check");
+
+    checkboxes
+        .append("input")
+        .attr("type", "checkbox")
+        .attr("id", d => d)
+        .attr("value", "")
+        .attr("class", "form-check-input")
+        .property("checked", true);
+
+    checkboxes
+        .append("label")
+        .attr("for", d => d)
+        .attr("class", "form-check-label")
+        .text(d => d);
 
 
+
+    let minHealthShare = d3.min(data, d => d.health_share);
+    let minFoodShare = d3.min(data, d => d.food_share)
     // Preprocessing the data
     data.forEach(function(d) {
       d.working_hours = +d.working_hours;
       d.population = +d.population;
       d.income_p_hour = +d.income_p_hour;
-      d.health_share = +d.health_share;
-      d.food_share = +d.food_share;
+      d.health_share = (d.health_share /(1-minHealthShare));
+      d.food_share = (d.food_share /(1-minFoodShare));
       d.gini = +d.gini;
     });
 
@@ -37,49 +61,78 @@ async function drawRadarGraph() {
 
     // Define radar chart function
     const drawChart = (data, index) => {
-      // Transform data to suitable representation
-      data = ["working_hours", "population", "income_p_hour", "health_share", "food_share", "gini"].map((metric, i, array) => ({
+        if (!document.getElementById(data.entity).checked) {
+            return;
+        }
+        // Transform data to suitable representation
+        data = ["working_hours", "population", "income_p_hour", "health_share", "food_share", "gini"].map((metric, i, array) => ({
         angle: (i / array.length) * 2 * Math.PI,
         radius: scales[metric](data[metric]),
-      }));
+        }));
 
-      // Append the first point to the end of data to close the line
-      data.push(data[0]);
+        // Append the first point to the end of data to close the line
+        data.push(data[0]);
 
-      // Create line generator
-      const line = d3.lineRadial()
-        .angle(d => d.angle)
-        .radius(d => d.radius);
+        // Create line generator
+        const line = d3.lineRadial()
+            .angle(d => d.angle)
+            .radius(d => d.radius);
 
-      // Append a path for each code
-      svg.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", d3.schemeCategory10[index % 10])  // Cycle through 10 colors
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
+        // Append a path for each code
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", d3.schemeCategory10[index % 10])  // Cycle through 10 colors
+            .attr("stroke-width", 1.5)
+            .attr("d", line);
+
+        AddAxis();
     };
+
+    d3.select("#select-all").on("click", function() {
+        d3.selectAll("#checkboxes input").property("checked", true);
+        svg.selectAll("*").remove();  // Clear all existing charts
+        data.forEach(drawChart);  // Redraw charts
+        AddAxis();
+    });
+
+// Event listener for "Deselect All" button
+    d3.select("#deselect-all").on("click", function() {
+        d3.selectAll("#checkboxes input").property("checked", false);
+        svg.selectAll("*").remove();  // Clear all existing charts
+        data.forEach(drawChart);  // Redraw charts
+        AddAxis();
+    });
 
     // Draw a radar chart for each code
     data.forEach(drawChart);
 
-    // Add axis lines and labels
-    const axisNames = ["working_hours", "population", "income_p_hour", "health_share", "food_share", "gini"];
-    for (let i = 0; i < axisNames.length; i++) {
-      let angle = (i / axisNames.length) * 2 * Math.PI;
-      svg.append("line")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", w/2 * Math.cos(angle - Math.PI/2))
-        .attr("y2", h/2 * Math.sin(angle - Math.PI/2))
-        .attr("stroke", "black");
+    d3.selectAll("#checkboxes input").on("change", function() {
+        svg.selectAll("*").remove();  // Clear all existing charts
+        data.forEach(drawChart);  // Redraw charts
+        AddAxis();
+    });
 
-      svg.append("text")
-        .attr("x", (w/2 + 10) * Math.cos(angle - Math.PI/2))
-        .attr("y", (h/2 + 10) * Math.sin(angle - Math.PI/2))
-        .attr("dy", ".35em")
-        .text(axisNames[i]);
+    function AddAxis(){
+        const axisNames = ["Working hours", "Population", "Income per hour", "Cheaper health cost", "Cheaper food cost", "Equality"];
+        for (let i = 0; i < axisNames.length; i++) {
+            let angle = (i / axisNames.length) * 2 * Math.PI;
+            svg.append("line")
+                .attr("x1", 0)
+                .attr("y1", 0)
+                .attr("x2", w/2 * Math.cos(angle - Math.PI/2))
+                .attr("y2", h/2 * Math.sin(angle - Math.PI/2))
+                .attr("stroke", "black");
+
+            svg.append("text")
+                .attr("x", (w/2 + 10) * Math.cos(angle - Math.PI/2))
+                .attr("y", (h/2 + 10) * Math.sin(angle - Math.PI/2))
+                .attr("dy", ".35em")
+                .text(axisNames[i]);
+        }
     }
+
+    d3.select('#radar-chart-placeholder').selectAll('*').remove();
 }
 
   
--- wrangling.py
+++ wrangling.py
@@ -175,7 +175,6 @@ df = pd.merge(df, gini_df, on=['code'], how='left')
 df.dropna(inplace=True)
 
 
-'''
 df['working_hours'] = 1/df['working_hours']
 df['health_share'] = 1/df['health_share']
 df['food_share'] = 1/df['food_share']
@@ -187,6 +186,7 @@ df['gini'] = 1/df['gini']
 df['population'] = 1/df['population']
 df['income_p_hour'] = 1/df['income_p_hour']
 
+'''
 
 
 print(df.head())
